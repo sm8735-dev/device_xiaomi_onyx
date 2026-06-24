30865646494d592ccdeb404fea642491c00758c5
zenin1504 <me.zenin1504.dev@gmail.com>
2026-06-24 16:25:10 +0800

    onyx: Required changes for CP2A
    * On sepolicy cil not needed on some roms
    
    Change-Id: I6430012ff5b745c6a5d477d73447e4c0
    Signed-off-by: zenin1504 <me.zenin1504.dev@gmail.com>

--- device.mk
+++ device.mk
@@ -259,13 +259,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
 
 # Lineage Health
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/xm_power/charger/smart_charge/smart_night)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
+#$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/xm_power/charger/smart_charge/smart_night)
+#$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+#$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+#$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
 
 # Media
 PRODUCT_COPY_FILES += \
