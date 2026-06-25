e83cef58648742ba1c20c90580aa0595d8e98c3a
zenin1504 <kayozzprojects@yandex.com>
2026-06-25 08:38:12 +0800

    onyx: Add necessary rom source patches needed for CP2A
    Change-Id: I2f1a49026f2044668f5d52ae7311af35
    Signed-off-by: zenin1504 <me.zenin1504.dev@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+cd device/qcom/sepolicy_vndr/sm8750
+git fetch https://github.com/sm8750-dev/device_qcom_sepolicy_vndr lineage-24.0-caf-sm8750
+git reset --hard FETCH_HEAD
+croot
+
+cd vendor/qcom/opensource/commonsys/audio
+git fetch https://github.com/sm8750-dev/vendor_qcom_opensource_audio lineage-24.0
+git reset --hard FETCH_HEAD
+croot
