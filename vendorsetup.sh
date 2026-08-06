36b7b38644bfc633f301cd17fee33cf4f398b9f7
zenin1504 <kayozzprojects@yandex.com>
2026-08-06 14:05:35 +0800

    onyx: Add yaap signing keys
    Change-Id: I504ff86f9230410ba9874fe576abf1a6
    Signed-off-by: zenin1504 <me.zenin1504.dev@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,7 @@
+# Signing keys
+rm -rf vendor/yaap/signing/keys
+git clone https://github.com/Neon-Duchamp/keys.git -b yaap-keys vendor/yaap/signing/keys
+
 # YAAP vendor
 cd vendor/yaap
 git fetch https://github.com/sm8735-dev/vendor_yaap sixteen
