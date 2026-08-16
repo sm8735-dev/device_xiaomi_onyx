46a9de02cfd8f29fa225cb5070b90e977cff8ee7
zenin1504 <kayozzprojects@yandex.com>
2026-08-16 12:47:52 +0800

    onyx: Cherry pick instead resetting hard
    Change-Id: I80d3900adf6a4a5ca17bb00970feafec
    Signed-off-by: zenin1504 <me.zenin1504.dev@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 # Fw/AV
 cd frameworks/av
 git fetch https://github.com/sm8735-dev/frameworks_av sixteen
-git reset --hard FETCH_HEAD
+git cherry-pick 56010073ba52d4b55683da314a3750c8e2e029fd
 croot
 
 # Signing keys
@@ -11,5 +11,5 @@ git clone https://github.com/Neon-Duchamp/keys.git -b yaap-keys vendor/yaap/sign
 # YAAP vendor
 cd vendor/yaap
 git fetch https://github.com/sm8735-dev/vendor_yaap sixteen
-git reset --hard FETCH_HEAD
+git cherry-pick 2a41fb363dc64d419875a3757b59b8b68d776128
 croot
