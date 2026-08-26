6bdb5243728adaeb224bace0f8a30e173349b2d8
zenin1504 <kayozzprojects@yandex.com>
2026-08-26 14:06:29 +0800

    onyx: Add audio ar source patch
    Change-Id: I9e179f777ba04cb39df00acec6101119
    Signed-off-by: zenin1504 <me.zenin1504.dev@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,9 @@
+# Audio
+cd hardware/qcom-caf/sm8750/audio/primary-hal
+git fetch https://github.com/sm8735-dev/hardware_qcom_audio-ar lineage-24.0-caf-sm8750
+git cherry-pick 98229065d5b0f22336707c81240825ba441acaa5
+croot
+
 # Compat
 cd hardware/lineage/compat
 git fetch https://github.com/sm8735-dev/hardware_lineage_compat seventeen
