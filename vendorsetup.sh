b0eda558ac04e04b76ed5f8742651647c5fdb136
zenin1504 <kayozzprojects@yandex.com>
2026-08-28 20:26:54 +0800

    onyx: Build Updater for unofficial builds
    Change-Id: I9c02b82bed884ddfba379a6780cbf145
    Signed-off-by: zenin1504 <me.zenin1504.dev@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,9 @@ cd hardware/qcom-caf/sm8750/audio/primary-hal
 git fetch https://github.com/sm8735-dev/hardware_qcom_audio-ar lineage-24.0-caf-sm8750
 git cherry-pick 98229065d5b0f22336707c81240825ba441acaa5
 croot
+
+# Voltage
+cd vendor/voltage
+git fetch https://github.com/sm8735-dev/vendor_voltage 17
+git cherry-pick 67ac8f99f0690c96d8295813feeb97c30072adad
+croot
