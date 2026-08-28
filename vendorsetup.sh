06186e5e2b09cbe61d06b0f62d353fe41c54a052
zenin1504 <kayozzprojects@yandex.com>
2026-08-28 19:21:32 +0800

    onyx: Drop some source patches
    Change-Id: I5de2ea734a6d48bd922366e0bb770b42
    Signed-off-by: zenin1504 <me.zenin1504.dev@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,25 +3,3 @@ cd hardware/qcom-caf/sm8750/audio/primary-hal
 git fetch https://github.com/sm8735-dev/hardware_qcom_audio-ar lineage-24.0-caf-sm8750
 git cherry-pick 98229065d5b0f22336707c81240825ba441acaa5
 croot
-
-# Compat
-cd hardware/lineage/compat
-git fetch https://github.com/sm8735-dev/hardware_lineage_compat seventeen
-git cherry-pick f770e5dff34084356d90a967b17d51a87e55fa0a
-croot
-
-# Fw/AV
-cd frameworks/av
-git fetch https://github.com/sm8735-dev/frameworks_av sixteen
-git cherry-pick 56010073ba52d4b55683da314a3750c8e2e029fd
-croot
-
-# Signing keys
-rm -rf vendor/yaap/signing/keys
-git clone https://github.com/Neon-Duchamp/keys.git -b yaap-keys vendor/yaap/signing/keys
-
-# YAAP vendor
-cd vendor/yaap
-git fetch https://github.com/sm8735-dev/vendor_yaap sixteen
-git cherry-pick 2a41fb363dc64d419875a3757b59b8b68d776128
-croot
