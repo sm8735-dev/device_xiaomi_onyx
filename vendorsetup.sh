73f3c17fb7d11e54386e9717f00b4ea1251b7ae1
zenin1504 <kayozzprojects@yandex.com>
2026-08-29 23:02:33 +0800

    onyx: Add more audio source patch for arpal-lx
    Change-Id: I6719b71714d045779f1eeeae67399c2c
    Signed-off-by: zenin1504 <me.zenin1504.dev@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,11 @@ git fetch https://github.com/sm8735-dev/hardware_qcom_audio-ar lineage-24.0-caf-
 git cherry-pick 98229065d5b0f22336707c81240825ba441acaa5
 croot
 
+cd hardware/qcom-caf/sm8750/audio/pal
+git fetch https://github.com/sm8735-dev/vendor_qcom_opensource_arpal-lx lineage-24.0-caf-sm8750
+git reset --hard FETCH_HEAD
+croot
+
 # Voltage
 cd vendor/voltage
 git fetch https://github.com/sm8735-dev/vendor_voltage 17
